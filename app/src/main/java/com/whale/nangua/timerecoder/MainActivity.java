5170cdce1ef097b13272ed8a83d5c7b98405edac
jiangzhengnan <impumpkin@qq.com>
2016-06-19 16:26:04 +0800

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/whale/nangua/timerecoder/MainActivity.java
+++ app/src/main/java/com/whale/nangua/timerecoder/MainActivity.java
@@ -1,13 +1,39 @@
 package com.whale.nangua.timerecoder;
 
+import android.content.Intent;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.whale.nangua.timerecoder.qr.activity.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.textbtn);
+        tv = (TextView) findViewById(R.id.resulttv);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CaptureActivity.class);
+                startActivityForResult(i,1);
+            }
+        });
+    }
+//
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String result = data.getStringExtra("result");
+
+        tv.setText(result);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
